b558dfa238948135dc8e25be82101877211dcee9
valdocs <blessedonesmus@gmail.com>
2022-09-14 06:13:26 -0700

    print standed error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "%s" ,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
