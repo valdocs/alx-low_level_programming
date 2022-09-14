0e75eb30bf074d826f29e64da354fd3a060bc3d7
valdocs <blessedonesmus@gmail.com>
2022-09-14 06:22:30 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,10 @@
 /**
  * main - Entry point
  *
- * Return: Always (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora
-		       	Korpar, 2015-10-19\n");
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
